6bdb4d869a0c1ea368ff2981034914d3bcd40d9e
adecess <axel.saliniere@protonmail.com>
2025-03-09 23:03:54 +0100

    implement database headers

--- src/main.c
+++ src/main.c
@@ -7,59 +7,87 @@
 #include "file.h"
 #include "parse.h"
 
-void print_usage(char *argv[]) {
+void print_usage(char *argv[])
+{
     printf("Usage: %s -n -f <database file>\n", argv[0]);
     printf("\t -n - create new database file\n");
     printf("\t -f - (required) path to database file\n");
     return;
 }
 
-int main(int argc, char *argv[]) { 
-	char *filepath = NULL;
+int main(int argc, char *argv[])
+{
+    char *filepath = NULL;
     bool newfile = false;
     int c;
 
     int dbfd = -1;
+    struct dbheader_t *dbhdr = NULL;
 
     // get option character from command line argument list
-    while((c = getopt(argc, argv, "nf:")) != -1) {
-        switch(c) {
-            case 'n':
-                newfile = true;
-                break;
-            case 'f':
-                // optarg points to the string following the option character f, the filepath in this case
-                filepath = optarg;
-                break;
-            case '?':
-                printf("Unknown option -%c\n", c);
-                break;
-            default:
-                return -1;
+    while ((c = getopt(argc, argv, "nf:")) != -1)
+    {
+        switch (c)
+        {
+        case 'n':
+            newfile = true;
+            break;
+        case 'f':
+            // optarg points to the string following the option character f, the filepath in this case
+            filepath = optarg;
+            break;
+        case '?':
+            printf("Unknown option -%c\n", c);
+            break;
+        default:
+            return -1;
         }
     }
 
-    if (filepath == NULL) {
+    if (filepath == NULL)
+    {
         printf("Filepath is a required argument\n");
         print_usage(argv);
 
         return 0;
     }
 
-    if (newfile) {
+    if (newfile)
+    {
         dbfd = create_db_file(filepath);
-        if (dbfd == -1) {
+        if (dbfd == -1)
+        {
             printf("Unable to create database file\n");
             return -1;
         }
-    } else {
+
+        if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR)
+        {
+            printf("Failed to create database header\n");
+            return -1;
+        }
+    }
+    else
+    {
         dbfd = open_db_file(filepath);
-        if (dbfd == -1) {
-            printf("Unable to create database file\n");
+
+        if (dbfd == -1)
+        {
+            printf("Unable to open database file\n");
+            return -1;
+        }
+
+        if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR)
+        {
+            printf("Failed to validate database header\n");
             return -1;
         }
     }
 
     printf("Newfile: %d\n", newfile);
     printf("Filepath: %s\n", filepath);
+
+    output_file(dbfd, dbhdr);
+
+    return 0;
 }
--- src/parse.c
+++ src/parse.c
@@ -10,28 +10,118 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
-
+void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
+{
 }
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
-
+int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring)
+{
 }
 
-int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
-
+int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut)
+{
 }
 
-int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
+int output_file(int fd, struct dbheader_t *dbhdr)
+{
+    if (fd < 0)
+    {
+        printf("Got a bad FD from user\n");
+        return STATUS_ERROR;
+    }
 
-}	
+    // data is converted from the host's native endianness to network byte order (big-endian)
+    dbhdr->magic = htonl(dbhdr->magic);
+    dbhdr->filesize = htonl(dbhdr->filesize);
+    dbhdr->count = htons(dbhdr->count);
+    dbhdr->version = htons(dbhdr->version);
 
-int validate_db_header(int fd, struct dbheader_t **headerOut) {
+    // reposition file cursor to the beginning of file
+    lseek(fd, 0, SEEK_SET);
 
+    // write the header back to the file on disk
+    write(fd, dbhdr, sizeof(struct dbheader_t));
+
+    return 0;
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
-	
+int validate_db_header(int fd, struct dbheader_t **headerOut)
+{
+    if (fd < 0)
+    {
+        printf("Got a bad FD from user\n");
+        return STATUS_ERROR;
+    }
+
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL)
+    {
+        printf("calloc failed to create a db header\n");
+        return STATUS_ERROR;
+    }
+
+    if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t))
+    {
+        perror("read");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    // The data in the header is in network byte order (big-endian) when you receive it 
+    // because that's how it was stored in the file.
+    // This is a common practice in binary file formats and network protocols
+    // to ensure consistency across different systems.
+
+    // While your program is working with the data,
+    // it uses the CPU's native endianness (host byte order),
+    // which is typically little-endian on most modern systems like x86/x86_64.
+    header->count = ntohs(header->count);
+    header->version = ntohs(header->version);
+    header->magic = ntohl(header->magic);
+    header->filesize = ntohl(header->filesize);
+
+    if (header->magic != HEADER_MAGIC)
+    {
+        printf("Improper header magic\n");
+        free(header);
+        return -1;
+    }
+
+    if (header->version != 1)
+    {
+        printf("Improper header version\n");
+        free(header);
+        return -1;
+    }
+
+    struct stat dbstat = { 0 };
+    fstat(fd, &dbstat);
+    if (header->filesize != dbstat.st_size)
+    {
+        printf("Corrupted database\n");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    // assign the header to the output pointer so the caller (main.c) can use it
+    *headerOut = header;
 }
 
+int create_db_header(int fd, struct dbheader_t **headerOut)
+{
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL)
+    {
+        printf("calloc failed to create db header pointer\n");
+        return STATUS_ERROR;
+    }
 
+    header->version = 1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *headerOut = header;
+
+    return STATUS_SUCCESS;
+}
